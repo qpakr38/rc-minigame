609bbab5cd9a43457e5fcbe7f7949d0a6124cd21
qpakr38 <zczc0819@gmail.com>
2023-03-26 12:03:16 +0900

    FEAT : 배경 그라데이션 추가

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
