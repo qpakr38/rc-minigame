abba8034b257520f8ff5f08715e6327c623c09ec
DONGWOOK KO <mfg.kdw@gmail.com>
2023-03-28 18:09:17 +0900

    FEAT : SafeAreaView를 활용한 노치 회피하기

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,20 @@
-import {Text} from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-const GameScreen=()=>{
-    return <Text>Game Screen!</Text>
-}
+const GameScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <Text>LOG ROUNDS</Text>
+    </View>
+  );
+};
 export default GameScreen;
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
